2ea9fc7d5db11a8dff693ecb0ad572c7befe73b8
Michael Froehlich <m.froehlich@tum.de>
2018-04-21 23:25:32 -0700

    Add prototypical VueJS application

---
+++ src/components/PopupContent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="popup-content" >
+    {{ label }}
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    props: {
+      label: {}
+    }
+  })
+</script>
+
+<style scoped>
+  .popup-content {
+    margin: 2px;
+    font-size: 10pt;
+  }
+</style>
