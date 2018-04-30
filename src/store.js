6fdb732542ec39f7307fee2829cb565b7dc033e6
Michael Froehlich <m.froehlich@tum.de>
2018-04-29 19:08:17 -0700

    Fix typo

--- src/store.js
+++ src/store.js
@@ -8,7 +8,7 @@ const HOST_POOL = [
   'https://dma18-final-project-server.herokuapp.com',
   'https://dma18-final-project-server1.herokuapp.com',
   'https://dma18-final-project-server2.herokuapp.com',
-  'https://dma18-final-project-server3.herokuapp.com',
+  'https://dma18-final-project-server3.herokuapp.com'
 ]
 const HOST = HOST_POOL[Math.floor(Math.random() * HOST_POOL.length)]
 
